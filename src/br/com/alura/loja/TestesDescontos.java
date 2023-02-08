7b7c6997ec37de82cf256cfe5181a7c1114cfe89
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-02-08 07:52:08 -0300

    Calculando descontos

---
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,17 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesDescontos {
+    public static void main(String[] args) {
+        Orcamento primeiro = new Orcamento(new BigDecimal("200"), 4);
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+        System.out.println(calculadora.calcular(primeiro));
+
+        Orcamento segundo = new Orcamento(new BigDecimal("1000"), 1);
+        System.out.println(calculadora.calcular(segundo));
+    }
+}
