4199a878f6896f2450136ab8fe3764432dec8d21
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-02-16 07:24:13 -0300

    API de registro de orçamento

---
+++ src/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+    public void registrar(Orcamento orcamento) {
+        // chamada HTTP para a API externa
+        // URL Connection
+        // Http Client
+        // lib Rest
+    }
+}
