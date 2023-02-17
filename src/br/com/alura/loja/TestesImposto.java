c19bf185fec4431a29f6f241fe155c07c2b371ef
Rogerio Mateus Ricardo <rogerio22mr@gmail.com>
2023-02-17 07:41:15 -0300

    Aplicando o pattern Decorator

--- src/br/com/alura/loja/TestesImposto.java
+++ src/br/com/alura/loja/TestesImposto.java
@@ -7,11 +7,11 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class TestesImpostos {
+public class TestesImposto {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, new ICMS(null)));
-        System.out.println(calculadora.calcular(orcamento, new ISS(null)));
+
+        System.out.println(calculadora.calcular(orcamento, new ICMS(new ISS(null))));
     }
 }
--- src/br/com/alura/loja/imposto/ISS.java
+++ src/br/com/alura/loja/imposto/ISS.java
@@ -4,8 +4,13 @@ import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
-public class ISS implements Imposto {
-    public BigDecimal calcular(Orcamento orcamento) {
+public class ISS extends Imposto {
+
+    public ISS(Imposto outro) {
+        super(outro);
+    }
+
+    public BigDecimal realizarCalculo(Orcamento orcamento) {
         return orcamento.getValor().multiply(new BigDecimal("0.06"));
     }
 }
